343b05125a152c8d34e0aee8f5cbca81411b185a
el-ngoca <eduardongoca4@gmail.com>
2023-02-20 14:19:23 -0800

    fprintf

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - This function prints using fprintf
+ *
+ * Return: 1 (sucsess)
+ */
+int main(void)
+{
+	fprintf("and that piece of art is useful\" - Dora Korpar, 2015-10-19");
+	return (1);
+}
