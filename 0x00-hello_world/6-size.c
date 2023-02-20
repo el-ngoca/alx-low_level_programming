531c18ce193fecedd28dde5951d83281e07dd291
el-ngoca <eduardongoca4@gmail.com>
2023-02-20 06:44:57 -0800

    a sizeof exercise

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ *main - A program that prints size of varios types
+ *
+ *Return: 0 (Sucsess)
+ */
+int main(void)
+{
+	printf("Size of a char:"sizeof(char));
+	printf("Size of an int:" sizeof(int));
+	printf("Size of a long int:" sizeof(long int));
+	printf("Size of a long long int:" sizeof(long long int));
+	printf("Size of a float:" sizeof(float));
+	return (0);
+}
