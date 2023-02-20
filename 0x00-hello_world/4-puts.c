bad5f0e1f48c99414506a618822ec1b22a4329cb
el-ngoca <eduardongoca4@gmail.com>
2023-02-20 04:27:06 -0800

    use puts to print string

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,6 @@
-
 #include <stdio.h>
 
-int man() {
-	puts("Programming is like building a multilingual puzzle");
+int main() {
+	puts("Programming is like building a multilingual puzzle\n");
 	return 0;
 }
