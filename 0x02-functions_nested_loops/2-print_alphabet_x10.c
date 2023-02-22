78c3a563b9fdf177967b5cd4ad739a329fafc42e
el-ngoca <eduardongoca4@gmail.com>
2023-02-22 03:04:02 -0800

    print alphabet

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * print_alphabet_x10 - this print the alphabet then times
+ */
+void print_alphabet_x10(void)
+{
+	char letter;
+	int i;
+	i=0;
+
+	while (i <= 10)
+	{ 
+	      letter= 'a';
+		while (letter <= 'z')
+		{ 
+			_putchar(letter);
+				letter=++;
+		}
+		_putchar('\n');
+		i++;
+	}
+}
