cfb4fa4c4a3ed99a163bde495794cda1c9ed73fc
el-ngoca <eduardongoca4@gmail.com>
2023-02-22 07:23:20 -0800

    absolet values

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * _abs - Thisprint absolet values
+ * @n: represent the numbers
+ * Return: (n), otherwise (-1)
+ */
+int _abs(int n)
+{
+	if (n >= 0)
+	{
+		return (n);
+	}
+return (-1)
+}
