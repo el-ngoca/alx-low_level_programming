da2be412f65e74b78d930278a02d7f66bfe6b9bb
el-ngoca <eduardongoca4@gmail.com>
2023-02-22 03:17:33 -0800

    print alphabet

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -6,17 +6,18 @@ void print_alphabet_x10(void)
 {
 	char letter;
 	int i;
-	i=0;
+
+	i = 0;
 
 	while (i < 10)
-	{ 
-	      letter= 'a';
-		while (letter <= 'z')
-		{ 
-			_putchar(letter);
-				letter++;
-		}
-		_putchar('\n');
-		i++;
+	{
+	      letter = 'a';
+	      while (letter <= 'z')
+	      { 
+		_putchar(letter);
+		letter++;
+	      }
+	_putchar('\n');
+	i++;
 	}
 }
