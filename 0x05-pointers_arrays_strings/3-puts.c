b0d16394a0ab851e8b6e96919ec80e04194c675d
el-ngoca <eduardongoca4@gmail.com>
2023-02-28 07:28:58 -0800

    adding a new line after str

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * _puts - this function print strint following by new line
+ * @str: input
+ */
+void _puts(char *str)
+{
+	while (*str != '\0')
+	{
+		_putchar(srt++);
+	}
+	_putchar('\n');
+}
