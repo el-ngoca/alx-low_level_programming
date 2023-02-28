7650e18efd7ca2aaaccb80bc84f75caab4b484fc
el-ngoca <eduardongoca4@gmail.com>
2023-02-28 09:56:09 -0800

    printhalf of  characters

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -6,15 +6,16 @@
 void puts_half(char *str)
 {
 	int o, n, l;
+	
 	l = 0;
 
-	for ( o = 0; str[o] != '\0'; o++)
+	for (o = 0; str[o] != '\0'; o++)
 	{
 		l++;
 	}
 	n = (l / 2);
 	if ((l % 2) == 1)
-		n = ((l+1) / 2);
+		n = ((l + 1) / 2);
 	for (o = n; str[o] != '\0'; o++)
 		_putchar(str[o]);
 	_putchar('\n');
