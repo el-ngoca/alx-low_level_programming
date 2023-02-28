e4a23e122a5eba6f2d53468e20fa2170640fe056
el-ngoca <eduardongoca4@gmail.com>
2023-02-28 07:49:12 -0800

    print in reversing order

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -5,10 +5,20 @@
  */
 void print_rev(char *s)
 {
+	int len = 0;
+	int c;
+
 	while (*s != '\0')
 	{
-		_putchar(*s--);
+		len++;
+		s++;
+	}
+	s--;
+	for (c = len; c > 0; c--)
+	{
+		_putchar(*s);
 		s--;
 	}
+
 	_putchar('\n');
 }
