3323195a0c0775efa80669d40702f2def0a35433
el-ngoca <eduardongoca4@gmail.com>
2023-02-28 08:59:36 -0800

    print all characters

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * puts2 - this will print every character of a string
+ * @str: input
+ */
+void puts2(char *str)
+{
+	int i =0;
+
+	while (str[i] != '\0')
+	{
+		printf("%c", str[i]);
+		i++;
+	}
+	printf("\n");
+}
