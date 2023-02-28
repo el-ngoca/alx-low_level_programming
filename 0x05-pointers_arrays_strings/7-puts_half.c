363d4b71a1e5d6fbcc799b1028c9e95cc1fd4f4e
el-ngoca <eduardongoca4@gmail.com>
2023-02-28 09:51:38 -0800

    printhalf of  characters

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * puts_half - this will print half off string
+ * @str: input
+ */
+void puts_half(char *str)
+{
+	int o, n, l;
+	l = 0;
+
+	for ( o = 0; str[o] != '\0'; o++)
+	{
+		l++;
+	}
+	n = (l / 2);
+	if ((l % 2) == 1)
+		n = ((l+1) / 2);
+	for (o = n; str[a] != '\0'; a++)
+		_putchar(str[a]);
+	_putchar('\n')
+}
