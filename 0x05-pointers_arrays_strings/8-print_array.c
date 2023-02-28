2eaf82298044158e409e97156e8369c81857dbfa
el-ngoca <eduardongoca4@gmail.com>
2023-02-28 10:44:27 -0800

    prints array element

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * print_array - this will print the elemment of array
+ * @a: input
+ * @n: input
+ */
+void print_array(int *a, int n)
+{
+int i;
+for (i = 0; i < n; i++)
+{
+	printf("%d", a[i]);
+	if (i != n-1)
+	{printf(",");
+	}
+}
+}
