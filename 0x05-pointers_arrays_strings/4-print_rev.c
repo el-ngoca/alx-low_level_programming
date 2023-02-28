09e42cdd230d7d66dc91c0d8d905acd8a0232158
el-ngoca <eduardongoca4@gmail.com>
2023-02-28 07:43:02 -0800

    print in reversing order

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * print_rev - thia function prints the string in reverse
+ * @s: input
+ */
+void print_rev(char *s)
+{
+	while (*s != '\0')
+	{
+		_putchar(*s--);
+		s--;
+	}
+	_putchat('\n')
+}
