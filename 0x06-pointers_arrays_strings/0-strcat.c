b6bf4e951a9dd1c61891b3ecd68c90550857aeb9
el-ngoca <eduardongoca4@gmail.com>
2023-03-01 15:36:14 -0800

    this will concat two functions

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -6,16 +6,15 @@
  * Return: dest (concatinated)
  */
 char *_strcat(char *dest, char *src)
-{
-	int dest_len = 0;
-	int i = 0;
-
-	while (dest[i++])
-	{
-		dest_len++;
-	}
-	for (i = 0;src[i]; i++)
-		dest[dest_len++] = src[i];
-
-	return (dest);
+char *_strcat(char *dest, const char *src)                                                                                            
+{                                                                                                                                    
+        int index = 0, dest_len = 0;                                                                                                 
+                                                                                                                                     
+        while (dest[index++])                                                                                                        
+                dest_len++;                                                                                                          
+                                                                                                                                     
+        for (index = 0; src[index]; index++)                                                                                         
+               dest[dest_len++] = src[index];                                                                                                                                                                   
+        return (dest);                                                                                                               
 }
+
