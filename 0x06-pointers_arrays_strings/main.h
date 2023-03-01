f57a847e811277b7472a5ca7d95cd67746985b6b
el-ngoca <eduardongoca4@gmail.com>
2023-03-01 15:40:22 -0800

    this will concat two functions

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -1,10 +1,10 @@
-#ifndef 
-#define 
+#ifndef MAIN_H
+#define MAIN_H
 
 #include <stdio.h>
 #include <unistd.h>
 
-int _putchar(char c)
+int _putchar(char c);
 char *_strcat(char *dest, char *src);
 char *_strncat(char *dest, char *src, int n);
 char *_strncpy(char *dest, char *src, int n);
