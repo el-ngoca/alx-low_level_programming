59d1b58b8c303ef99612e6e3085d462b0c3d3d6f
el-ngoca <eduardongoca4@gmail.com>
2023-03-01 15:57:16 -0800

    this will concat two functions

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -3,14 +3,22 @@
  * _strncat - Concatenates two strings using at most   
  * @dest: The string to be appended upon.                                        * @src: The string to be appended   
  * @n: The number of bytes from src to be appended to dest.             
- * Return: A pointer to the resulting string dest.                                                                                   
- */                                                                                                                                  
-char *_strncat(char *dest, char *src, int n)                                                                                         
-{                                                                                                                                    
-        int index = 0, dest_len = 0;
-	while (dest[index++])
-		dest_len++;
-	for (index = 0; src[index] && index < n; index++)                                                                            
-                dest[dest_len++] = src[index];
+ * Return: A pointer to the resulting string dest.                               */                                                                                                                                  
+char *_strncat(char *dest, char *src, int n)                                    int i;
+	int j;
+
+	i = 0;
+	while (dest[i] != '\0')
+	{
+		i++;
+	}
+	j = 0;
+	while (j < n && src[j] != '\0')
+	{
+	dest[i] = src[j];
+	i++;
+	j++;
+	}
+	dest[i] = '\0';
 	return (dest);
 }
