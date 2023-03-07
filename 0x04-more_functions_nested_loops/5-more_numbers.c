2f849c89a2be2fecba15f05e5c176a2e63d56120
el-ngoca <eduardongoca4@gmail.com>
2023-03-06 16:01:48 -0800

    print  m

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -1,5 +1,4 @@
 #include "main.h"
-
 /**
  * more_numbers - prints 10 times the numbers, from 0 to 14
  * followed by a new line
@@ -17,4 +16,5 @@ _putchar(j / 10 + '0');
 _putchar(j % 10 + '0');
 }
 _putchar('\n');
+}
 }
