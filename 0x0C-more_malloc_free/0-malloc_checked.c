0502d41ea6b8306be15cdce31eed1ad9b2d91c00
el-ngoca <eduardongoca4@gmail.com>
2023-11-23 16:14:21 -0800

    adding the first file

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <stdlib.h>
+/**
+ *malloc_checked - returns a ponter to that memory
+ *@b: input data entry
+ *
+ */
+void *malloc_checked(unsigned int b)
+{
+	void *s;
+
+	s = malloc(sizeof(b));
+
+	if(s == NULL)
+		exit (98);
+	return (s);
+}
